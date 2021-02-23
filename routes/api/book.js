169f05bdaf0b2485dc8ab899e4e77ae26efbf5d0
That Guy Cal <calmanning@gmail.com>
2021-02-22 22:34:42 -0800

    added framework.

---
+++ routes/api/book.js
@@ -0,0 +1,5 @@
+//google books API key https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyB3YZciivXGvfjkTT78-Imc5GFoknQ94ic
+//"q=flowers" = subject "+inAuthor:keyes" = the author, Keyes
+
+//https://www.googleapis.com/books/v1/volumes?q=intitle:quiver&key=AIzaSyB3YZciivXGvfjkTT78-Imc5GFoknQ94ic
+//searches only based on title. "intitle"
---
+++ src/pages/Search.js
@@ -0,0 +1,20 @@
+
+
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+function Search() {
+  return (
+    <div >
+      More testing 
+      <form>
+      <Input
+        onChange={() => {}}
+        name="title"
+        placeholder="Search by book title"
+      />
+      </form>
+    </div>
+  );
+}
+
+export default Search;
